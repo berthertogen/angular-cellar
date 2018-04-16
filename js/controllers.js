39086e75657f19ab75242e9a6a3eb334ee2719c7
Bert Hertogen <bert.hertogen@live.be>
2018-04-16 08:53:07 +0200

    Added books

--- js/controllers.js
+++ js/controllers.js
@@ -3,6 +3,7 @@ function RouteCtrl($route) {
     var self = this;
 
     $route.when('/wines', {template:'tpl/welcome.html'});
+    $route.when('/books', {template:'tpl/welcome-book.html'});
 
     $route.when('/wines/:wineId', {template:'tpl/wine-details.html', controller:WineDetailCtrl});
 
@@ -22,13 +23,27 @@ function RouteCtrl($route) {
 
 function WineListCtrl(Wine) {
 
-    this.wines = Wine.query();
+    // this.wines = Wine.query();
+    this.wines = [{
+        id: 1,
+        name: 'Test 1'
+    }, {
+        id: 2,
+        name: 'Test 2'
+    }];
 
 }
 
 function WineDetailCtrl(Wine) {
 
-    this.wine = Wine.get({wineId:this.params.wineId});
+    // this.wine = Wine.get({wineId:this.params.wineId});
+    this.wine = {
+        id: 1,
+        name: 'Test',
+        grapes: 'Test Grapes',
+        year: 2018,
+        description: 'dfsdf ds fdsf sd fdsf dsf dsfds fsd'
+    };
 
 
     this.saveWine = function () {
